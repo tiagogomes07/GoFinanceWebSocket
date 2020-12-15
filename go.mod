6c0efc88b8231f5f4249bae531a3add5c50f5be6
tiagogomes07 <tiagogomes07@gmail.com>
2020-12-14 23:00:39 -0300

    method get stock

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ module GoSocket
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/googollee/go-socket.io v1.4.4
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/lib/pq v1.9.0 // indirect
 	github.com/rs/cors v1.7.0
--- main.go
+++ main.go
@@ -1,107 +1,82 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
-	"os"
 
-	socketio "github.com/googollee/go-socket.io"
-	"github.com/googollee/go-socket.io/engineio"
-	"github.com/googollee/go-socket.io/engineio/transport"
-	"github.com/googollee/go-socket.io/engineio/transport/polling"
-	"github.com/googollee/go-socket.io/engineio/transport/websocket"
+	"fmt"
 
-	"github.com/gorilla/mux"
-
-	"github.com/rs/cors"
-
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	server, err := socketio.NewServer(&engineio.Options{
-		Transports: []transport.Transport{
-			polling.Default,
-			&websocket.Transport{
-				CheckOrigin: func(r *http.Request) bool {
-					return false
-				},
-			},
-		},
-	})
+var addr = flag.String("addr", "localhost:5000", "http service address")
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println("error ini")
+		log.Print("upgrade:", err)
+		return
 	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
 
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		return nil
-	})
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
+func getStock(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
 
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
+		
 
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
 
-	server.OnError("/", func(s socketio.Conn, e error) {
-		fmt.Println("meet error:", e)
-	})
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		fmt.Println("closed", reason)
-	})
-
-	go server.Serve()
-	defer server.Close()
-
-	mux := mux.NewRouter()
-
-	mux.Handle("/socket.io/", server)
-	mux.HandleFunc("/", index)
-
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	w.Write([]byte("{\"hello\": \"world\"}"))
-
-	// 	//index(w, r)
-	// })
-
-	//
-
-	// log.Println("Serving at localhost:PORT " + PORT)
-	// //cors.AllowAll().Handler(mux))
-	// log.Fatal(http.ListenAndServe(PORT, mux))
-
-	handler := cors.Default().Handler(mux)
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-	})
-
-	handler = c.Handler(handler)
-
-	godotenv.Load(".env")
-	PORT := ":" + os.Getenv("PORT")
-
-	log.Println("Serving at localhost" + PORT + "...")
-	log.Fatal(http.ListenAndServe(PORT, handler))
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/ws", getStock)
+	http.HandleFunc("/", index)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
